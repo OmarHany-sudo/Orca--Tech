6e1dc3febaf2f22a77fa82576b9d426e7ae5f5d7
Omar hany shalaby <S23511739EG@Std.aou.edu.eg>
2025-11-22 16:23:50 +0200

    Change logo source based on scroll state

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -44,7 +44,7 @@ export default function Navbar() {
         {/* Logo */}
         <Link href="/" className="flex items-center">
           <img
-            src={isScrolled ? '/logo.jpg' : '/logo.jpg'}
+            src={isScrolled ? '/logo.jpg' : '/logo2.jpg'}
             alt="OrcaTech Logo"
             className="h-10 w-auto transition-all duration-300"
           />
