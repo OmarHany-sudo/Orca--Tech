61027359e075eeab37f8f199ba89959cfcebda1e
Omar hany shalaby <S23511739EG@Std.aou.edu.eg>
2026-02-05 18:40:08 +0200

    Create privacy-policy/page.tsx

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,18 @@
+Privacy Policy – OrcaTech
+
+At OrcaTech, we respect your privacy and are committed to protecting your personal data.
+
+We collect the following information when you submit our forms:
+- Name
+- Email address
+- Phone number
+
+This information is used only to:
+- Contact you regarding your request
+- Provide our services and consultations
+
+We do NOT share, sell, or rent your data to any third party.
+
+By submitting your information, you agree to this Privacy Policy.
+
+If you have any questions, you can contact us through our official channels.
