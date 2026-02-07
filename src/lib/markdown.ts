4da18ffa4787d2af66754c86688e6cdcb3955a4d
Omar hany shalaby <S23511739EG@Std.aou.edu.eg>
2026-02-07 04:47:41 +0200

    Update markdown.ts

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,5 +1,9 @@
-import { marked } from "marked";
-
 export function markdownToHtml(md: string) {
-  return marked.parse(md);
+  return md
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>')
+    .replace(/\*(.*)\*/gim, '<em>$1</em>')
+    .replace(/\n$/gim, '<br />');
 }
